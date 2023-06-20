57c3479605e7de2882ad926bbc5d65049e9e545b
579ankit <53573220+579ankit@users.noreply.github.com>
2023-06-20 10:55:08 +0000

    [LeetCode Sync] Runtime - 785 ms (9.78%), Memory - 0.0B (100.00%)

--- DSA/0181-employees-earning-more-than-their-managers/solution.sql
+++ DSA/0181-employees-earning-more-than-their-managers/solution.sql
@@ -1,1 +1,2 @@
-select m.name as Employee from employee e inner join employee m where e.id=m.managerid and e.salary<m.salary;
+select a1.name as Employee from employee a1 join employee a2 where a1.managerid=a2.id and a1.salary>a2.salary ;
+
