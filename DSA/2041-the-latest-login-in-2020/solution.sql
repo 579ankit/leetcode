9519ec796eb23cf32258e74a91e3b310984e69f8
579ankit <53573220+579ankit@users.noreply.github.com>
2023-06-22 10:44:58 +0000

    [LeetCode Sync] Runtime - 1500 ms (5.01%), Memory - 0.0B (100.00%)

--- DSA/2041-the-latest-login-in-2020/solution.sql
+++ DSA/2041-the-latest-login-in-2020/solution.sql
@@ -1,1 +1,2 @@
-select user_id,max(time_stamp) as last_stamp from logins where time_stamp like '2020%' group by user_id order by last_stamp desc
+select user_id,max(time_stamp) last_stamp from logins where time_stamp like '2020%' group by user_id
+
