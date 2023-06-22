d83133635c548d2c4b3086d6ebc4637f0bb04408
579ankit <53573220+579ankit@users.noreply.github.com>
2023-06-22 11:55:03 +0000

    [LeetCode Sync] Runtime - 1618 ms (5.00%), Memory - 0.0B (100.00%)

--- DSA/1877-find-followers-count/solution.sql
+++ DSA/1877-find-followers-count/solution.sql
@@ -1,1 +1,1 @@
-select user_id,count(follower_id) as followers_count from followers group by user_id order by user_id;
+select user_id,count(user_id) followers_count from followers group by user_id order by user_id
