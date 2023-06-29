779f83a1caeb8477a426a5856b5c33b2324cd7f1
579ankit <53573220+579ankit@users.noreply.github.com>
2023-06-29 05:11:08 +0000

    [LeetCode Sync] Runtime - 1142 ms (5.01%), Memory - 0.0B (100.00%)

---
+++ DSA/1942-primary-department-for-each-employee/solution.sql
@@ -0,0 +1,13 @@
+WITH A AS (
+    SELECT employee_id
+    FROM Employee
+    GROUP BY employee_id
+    HAVING COUNT(department_id) = 1
+)
+
+SELECT employee_id, department_id
+FROM Employee
+WHERE employee_id in (
+    SELECT *
+    FROM A
+) OR primary_flag = 'Y'
