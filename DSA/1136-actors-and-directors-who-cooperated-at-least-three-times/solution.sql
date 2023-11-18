c98e5634c36475841cb120efa8a1ec12e3e08ed7
579ankit <53573220+579ankit@users.noreply.github.com>
2023-11-18 14:56:54 +0000

    [LeetCode Sync] Runtime - 770 ms (5.00%), Memory - 0.0B (100.00%)

--- DSA/1136-actors-and-directors-who-cooperated-at-least-three-times/solution.sql
+++ DSA/1136-actors-and-directors-who-cooperated-at-least-three-times/solution.sql
@@ -1,3 +1,1 @@
-select actor_id,director_id from actordirector group by actor_id,director_id having count(actor_id) >=3
-
-
+select actor_id,director_id from actordirector group by actor_id,director_id having count(director_id) >=3 
