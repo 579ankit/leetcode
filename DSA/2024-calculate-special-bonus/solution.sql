6c8213bd72a84811f74d1ab3eee542c57b74e331
579ankit <53573220+579ankit@users.noreply.github.com>
2023-11-20 09:16:00 +0000

    [LeetCode Sync] Runtime - 1182 ms (5.00%), Memory - 0.0B (100.00%)

--- DSA/2024-calculate-special-bonus/solution.sql
+++ DSA/2024-calculate-special-bonus/solution.sql
@@ -1,1 +1,7 @@
-select employee_id,if(employee_id%2!=0 and name not like 'M%',salary,0) bonus from employees order by employee_id
+select employee_id,
+case
+when employee_id %2 !=0 and name not like 'M%' then salary
+when employee_id %2 !=0 and name like 'M%' then salary = 0
+when employee_id %2 =0 then salary = 0
+end as bonus
+from  employees order by employee_id
