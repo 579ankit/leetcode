3eededf05c7c42e591ba5c884dd7916b0c0e54c2
579ankit <53573220+579ankit@users.noreply.github.com>
2023-11-22 04:05:11 +0000

    [LeetCode Sync] Runtime - 438 ms (10.40%), Memory - 0.0B (100.00%)

--- DSA/0570-managers-with-at-least-5-direct-reports/solution.sql
+++ DSA/0570-managers-with-at-least-5-direct-reports/solution.sql
@@ -1,2 +1,6 @@
-select e1.name from employee e1,employee e2 where e1.id=e2.managerid group by e1.name having count(*)>=5
+select e1.name
+from employee e1 join employee e2
+on e1.id = e2.managerId
+group by e2.managerid 
+having count(e2.managerid) >= 5
 
