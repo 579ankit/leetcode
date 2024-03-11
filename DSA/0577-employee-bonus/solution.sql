8f375afe77fd146c30f267e42938ccc7e08a343d
579ankit <53573220+579ankit@users.noreply.github.com>
2024-03-11 11:51:01 +0000

    [LeetCode Sync] Runtime - 2248 ms (5.02%), Memory - 0.0B (100.00%)

--- DSA/0577-employee-bonus/solution.sql
+++ DSA/0577-employee-bonus/solution.sql
@@ -1,5 +1,4 @@
-select e.name,b.bonus from
-employee e left join bonus b
-on e.empid = b.empid
+select e.name,b.bonus from employee e
+left join bonus b on e.empid=b.empid 
 where b.bonus<1000
 or b.bonus is null;
