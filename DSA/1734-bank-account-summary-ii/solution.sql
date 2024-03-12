acad1708aab2c76d58a2f05149744d55635b2ca5
579ankit <53573220+579ankit@users.noreply.github.com>
2024-03-12 10:36:50 +0000

    [LeetCode Sync] Runtime - 1417 ms (5.02%), Memory - 0.0B (100.00%)

--- DSA/1734-bank-account-summary-ii/solution.sql
+++ DSA/1734-bank-account-summary-ii/solution.sql
@@ -1,3 +1,6 @@
-select u.name,sum(t.amount) as balance from users u join transactions t on u.account = t.account group by t.account having sum(t.amount) > 10000
-
-
+select u.name,sum(t.amount)
+balance from users u join 
+transactions t
+on u.account=t.account
+group by u.account
+having sum(t.amount)>10000;
