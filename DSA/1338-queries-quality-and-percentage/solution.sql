7169626a88dc7581a63af9f7e4648823d5b15997
579ankit <53573220+579ankit@users.noreply.github.com>
2024-08-06 10:13:01 +0000

    [LeetCode Sync] Runtime - 411 ms (21.43%), Memory - 0.0B (100.00%)

--- DSA/1338-queries-quality-and-percentage/solution.sql
+++ DSA/1338-queries-quality-and-percentage/solution.sql
@@ -1,4 +1,7 @@
-select query_name,round(sum(rating/position)/count(query_name),2) as quality,round(sum(if(rating<3,1,0))*100/count(rating),2) as poor_query_percentage from queries group by query_name
+select query_name,
+round(sum(rating/position)/count(query_name),2) quality,
+round(sum(if(rating<3,1,0))*100/count(rating),2) as poor_query_percentage 
+from queries where query_name is not null group by query_name ;
 
 
 
