3d443ea5bfb4b5e7eb95c360c63b6f04c5a879eb
579ankit <53573220+579ankit@users.noreply.github.com>
2024-08-07 13:14:38 +0000

    [LeetCode Sync] Runtime - 1273 ms (43.25%), Memory - 0.0B (100.00%)

--- DSA/1509-replace-employee-id-with-the-unique-identifier/solution.sql
+++ DSA/1509-replace-employee-id-with-the-unique-identifier/solution.sql
@@ -1,3 +1,3 @@
-select e2.unique_id,e1.name
-from employees e1 left join 
-employeeuni e2 on e1.id=e2.id;
+select ifnull(e2.unique_id,null) as unique_id,e1.name 
+from employees e1 left join employeeuni e2
+on e1.id=e2.id;
