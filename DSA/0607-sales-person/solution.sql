50c0a2971d838261cf1422d2fa21a6632bad87d0
579ankit <53573220+579ankit@users.noreply.github.com>
2025-01-03 08:55:59 +0000

    [LeetCode Sync] Runtime - 2403 ms (5.01%), Memory - 0.0B (100.00%)

--- DSA/0607-sales-person/solution.sql
+++ DSA/0607-sales-person/solution.sql
@@ -1,9 +1,4 @@
-SELECT name
-FROM salesperson where name not in
-(SELECT s.name
-FROM salesperson s 
-join Orders o 
-on s.sales_id = o.sales_id
-join company c 
-on c.com_id = o.com_id 
-where c.name='RED')
+select name from salesperson where name not in 
+(select s.name from salesperson s left join orders o
+on s.sales_id = o.sales_id join company c on c.com_id = o.com_id where c.name='RED')
+
