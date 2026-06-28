dccaa5a74b143ea13a7f786807460541f5903ebf
579ankit <53573220+579ankit@users.noreply.github.com>
2026-06-28 10:22:43 +0000

    [LeetCode Sync] Runtime - 268 ms (45.44%), Memory - 16.8 MB (98.30%)

--- DSA/0940-fruit-into-baskets/solution.py
+++ DSA/0940-fruit-into-baskets/solution.py
@@ -4,20 +4,16 @@ class Solution(object):
         :type fruits: List[int]
         :rtype: int
         """
-        low=0
+        low,n=0,len(fruits)
         freq={}
-        n=len(fruits)
         res=0
         for high in range(n):
-            fruit=fruits[high]
-            freq[fruit]=freq.get(fruit,0)+1
+            freq[fruits[high]]=freq.get(fruits[high],0)+1
             while len(freq)>2:
                 freq[fruits[low]]-=1
                 if freq[fruits[low]]==0:
                     del freq[fruits[low]]
                 low+=1
             length=high-low+1
-            res=max(length,res)
+            res=max(res,length)
         return res
-
-
