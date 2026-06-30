6f68ad2a30d694f0a4a7757cf3a638b6f5ae9ef7
579ankit <53573220+579ankit@users.noreply.github.com>
2026-06-30 15:56:17 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.5 MB (17.03%)

--- DSA/0202-happy-number/solution.py
+++ DSA/0202-happy-number/solution.py
@@ -1,11 +1,11 @@
 class Solution(object):
+
     def fun(self,n):
         s=0
         while n>0:
             d=n%10
-            s=s+d*d
-            n=n//10
-            
+            s+=d*d
+            n//=10
         return s
 
     def isHappy(self, n):
@@ -20,7 +20,5 @@ class Solution(object):
             fast=self.fun(fast)
             if slow==fast and slow!=1:
                 return False
-        fast=1
         return True
-
 
