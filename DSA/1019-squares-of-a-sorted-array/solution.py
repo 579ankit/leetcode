9f3e030dbfcfebc5f9ead17a85d8a0f0f1a2d47a
579ankit <53573220+579ankit@users.noreply.github.com>
2026-06-30 10:14:01 +0000

    [LeetCode Sync] Runtime - 13 ms (53.15%), Memory - 14.1 MB (91.77%)

--- DSA/1019-squares-of-a-sorted-array/solution.py
+++ DSA/1019-squares-of-a-sorted-array/solution.py
@@ -5,18 +5,17 @@ class Solution(object):
         :rtype: List[int]
         """
         n=len(nums)
+        i,j=0,n-1
         a=[0]*n
-        i,j,k=0,n-1,n-1
+        k=n-1
         while i<=j:
             isq=nums[i]*nums[i]
             jsq=nums[j]*nums[j]
-            if isq<=jsq:
-                a[k]=jsq
-                j-=1
-            else:
+            if isq>jsq:
                 a[k]=isq
                 i+=1
+            else:
+                a[k]=jsq
+                j-=1
             k-=1
         return a
-
-
